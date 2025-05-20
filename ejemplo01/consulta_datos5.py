079d3e7e7cd59db2f5b145ab127127bb65300bc7
Soffivp <psvire@utpl.edu.ec>
2025-05-20 09:04:51 -0500

    cambios

--- ejemplo01/consulta_datos5.py
+++ ejemplo01/consulta_datos5.py
@@ -25,26 +25,6 @@ session = Session()
 # para la solución se hace uso del método 
 # join aplicado a query
 
-clubs = session.query(Club).join(Jugador).\
-        filter(Jugador.nombre.like("%Da%")).all()
-# print(clubs)
-print("Consulta 1 ")
-"""
-Consulta 1 
-Club: nombre=Barcelona deporte=Fútbol fundación=1920
-"""
-for e in clubs:   
-    print(e) 
-
-# Obtener un listado de todos los registros 
-# de la tabla Club y Jugador, que tengan al menos 
-# un jugador con el nombre que tenga incluida la cadena “Da”
-
-# para la solución se hace uso del método 
-# join aplicado a query
-# en el query se ubican las dos entidades involucradas
-# 
-
 registros = session.query(Club, Jugador).join(Jugador).\
          filter(Jugador.nombre.like("%Da%")).all()
  
